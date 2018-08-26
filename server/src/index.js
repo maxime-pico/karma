eb04ffde489935f9a7c243482299fa096ae62d5e
Green-Dirt <greendirt.karmapanda@protonmail.com>
2018-08-26 16:10:08 +0200

    fixes some bootstraping configs

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-// 1
-const typeDefs = `
-type Query {
-  karma: Int!
-}
-`
-
 // 2
 const resolvers = {
   Query: {
@@ -16,7 +9,7 @@ const resolvers = {
 
 // 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
