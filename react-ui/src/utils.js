0a09975282d8d6e62bdb492654360f240ef6337f
Green-Dirt <greendirt.karmapanda@protonmail.com>
2018-11-01 11:54:41 +0100

    desactivates url check on form submit until better regex is found

--- react-ui/src/utils.js
+++ react-ui/src/utils.js
@@ -31,8 +31,9 @@ export function adjacentAct(cause, act, direction) {
 }
 
 export function isUrlValid(userInput) {
-	var regexQuery =
-		'^(https?://)?(www\\.)?([-a-z0-9]{1,63}\\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.[a-z]{2,6}(/[-\\w@\\+\\.~#\\?&/=%]*)?$'
-	var url = new RegExp(regexQuery, 'i')
-	return url.test(userInput)
+	// var regexQuery =
+	// 	'^(https?://)?(www\\.)?([-a-z0-9]{1,63}\\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.[a-z]{2,6}(/[-\\w@\\+\\.~#\\?&/=%]*)?$'
+	// var url = new RegExp(regexQuery, 'i')
+	// return url.test(userInput)
+	return true
 }
