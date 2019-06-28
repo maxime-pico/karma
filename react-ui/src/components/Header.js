ae1deb100929cef7806466fbb86b6ad45f77fec4
Green-Dirt <greendirt.karmapanda@protonmail.com>
2019-06-28 14:50:23 +0200

    Styles Navbar header and background

--- react-ui/src/components/Header.js
+++ react-ui/src/components/Header.js
@@ -3,77 +3,52 @@ import Cookies from 'universal-cookie'
 import { Link } from 'react-router-dom'
 import { withRouter } from 'react-router'
 import { AUTH_TOKEN } from '../constants'
-import karmalogo from '../images/logo.png'
+import karmalogo from '../images/logo-small.svg'
 import UserBubble from './UserBubble'
-import CompanyOverview from './CompanyOverview'
 import styled from 'styled-components'
 import { Grid, Row, Col } from '@smooth-ui/core-sc'
 
-const HeaderBubble = styled.div`
+const Navbar = styled(Row)`
+	color: #a9b4cc;
+	padding: 12px 42px;
+	font-size: 14px;
 	background-color: white;
-	padding: 30px 50px 30px 40px;
-	margin: 0 40px 0 0;
-	border-radius: 0 0 100px 0;
-	box-shadow: inset -8px -3px 20px #80808040;
+	text-align: left;
+	margin-bottom: 96px;
 `
-const HomeButton = styled.div`
-	padding-top: 3px;
-	font-weight: 600;
-	font-size: 1.1em;
-`
-const TopRightCorner = styled.div`
-	color: white;
-	padding: 3px 30px 0 0;
-	font-weight: 600;
-	font-size: 1.1em;
+
+const NavbarLink = styled(Link)`
+	margin: auto;
+	margin: auto 0 auto 42px;
 `
 
 class Header extends React.Component {
 	render() {
 		const cookies = new Cookies() // get access to cookies
 		const authToken = cookies.get(AUTH_TOKEN) // if user is logged in authToken now contains the token
-		const match = this.props.location.pathname.match(/\/company\/(.*?)(\/|$)/)
-		const companyId = match ? match[1] : null
 		return (
 			<Grid fluid>
-				<Row>
+				<Navbar>
 					<Col p={0} style={{ marginRight: 'auto' }}>
-						<div
-							style={{
-								display: 'flex',
-								justifyContent: 'flex-start',
-							}}
-						>
-							<HeaderBubble>
-								{companyId ? (
-									<CompanyOverview companyId={companyId} />
-								) : (
-									<img src={karmalogo} height="60" alt="karma panda" />
-								)}
-							</HeaderBubble>
-						</div>
+						<Link to="/">
+							<img src={karmalogo} height="35" alt="karma panda" />
+						</Link>
 					</Col>
-					<Col p={0}>
-						<HomeButton>
-							<Link to="/" style={{ color: 'white' }}>
-								Toutes les entreprises
-							</Link>
-						</HomeButton>
-					</Col>
-					<Col p={0} style={{ marginLeft: 'auto' }}>
+					<Col p={0} style={{ alignSelf: 'center' }}>
 						<div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-							<TopRightCorner>
-								{authToken ? (
-									<UserBubble />
-								) : (
-									<Link to="/login" className="ml1 no-underline black">
-										Se connecter ou S'incrire
-									</Link>
-								)}
-							</TopRightCorner>
+							<NavbarLink to="/help">aide</NavbarLink>
+							<NavbarLink to="/about">à propos</NavbarLink>
+							<NavbarLink to="/brands">marques</NavbarLink>
+							{authToken ? (
+								<UserBubble />
+							) : (
+								<NavbarLink to="/login" className="ml1 no-underline black">
+									connexion
+								</NavbarLink>
+							)}
 						</div>
 					</Col>
-				</Row>
+				</Navbar>
 			</Grid>
 		)
 	}
