7019274778c176caccdd5fbb462cf5caa7988303
Green-Dirt <greendirt.karmapanda@protonmail.com>
2019-08-05 09:13:40 +0200

    Hides profile navbar item on mobile

--- react-ui/src/components/navbar/Navbar.js
+++ react-ui/src/components/navbar/Navbar.js
@@ -15,22 +15,22 @@ const NavbarBox = styled(Row)`
 	background-color: white;
 	text-align: left;
 	@media (max-width: 425px) {
-    	padding: 0;
-  	}
+		padding: 0;
+	}
 `
 
 const NavbarLink = styled(Link)`
 	margin: auto;
 	margin: auto 0 auto 42px;
 	@media (max-width: 425px) {
-    	width: 100%;
-    	height: 40px;
-    	line-height : 40px;
-    	margin: auto;
-    	text-align: center;
-    	border-top: 1px solid rgba(169, 180, 204, .7);
-    	//display: block;
-  	}
+		width: 100%;
+		height: 40px;
+		line-height: 40px;
+		margin: auto;
+		text-align: center;
+		border-top: 1px solid rgba(169, 180, 204, 0.7);
+		//display: block;
+	}
 `
 
 const NavbarLinkContainer = styled.div`
@@ -38,7 +38,7 @@ const NavbarLinkContainer = styled.div`
 	justify-content: flex-end;
 	@media (max-width: 425px) {
 		flex-flow: column wrap;
-		display: ${props => props.displayValue}
+		display: ${props => props.displayValue};
 	}
 `
 
@@ -50,34 +50,33 @@ const LogoContainer = styled(Col)`
 	}
 `
 const BurgerBar = styled.div`
-  width: 25px;
-  height: 2px;
-  background-color: rgba(169, 180, 204, .7);
-  margin: 6px 0;
+	width: 25px;
+	height: 2px;
+	background-color: rgba(169, 180, 204, 0.7);
+	margin: 6px 0;
 `
 
 const BurgerMenu = styled(Col)`
-  display: none;
-  @media (max-width: 425px) {
+	display: none;
+	@media (max-width: 425px) {
 		display: block;
-  }
+	}
 `
 
-
 class Navbar extends React.Component {
-	constructor(){
-        super();
-        this.state = {
-           isBurgerOpen: false
-        }
-    }
+	constructor() {
+		super()
+		this.state = {
+			isBurgerOpen: false,
+		}
+	}
 	_toggleBurger() {
-		this.setState({isBurgerOpen: !this.state.isBurgerOpen})
+		this.setState({ isBurgerOpen: !this.state.isBurgerOpen })
 	}
 	render() {
 		const cookies = new Cookies() // get access to cookies
 		const authToken = cookies.get(AUTH_TOKEN) // if user is logged in authToken now contains the token
-		let burgerDisplayProp = this.state.isBurgerOpen ? "flex" : "none";
+		let burgerDisplayProp = this.state.isBurgerOpen ? 'flex' : 'none'
 		return (
 			<Grid fluid>
 				<NavbarBox>
@@ -86,23 +85,29 @@ class Navbar extends React.Component {
 							<img src={karmalogo} height="35" alt="karma panda" />
 						</Link>
 					</LogoContainer>
-					<BurgerMenu xs={2} style={{ alignSelf: 'center'}} onClick={this._toggleBurger.bind(this)}>
+					<BurgerMenu
+						xs={2}
+						style={{ alignSelf: 'center' }}
+						onClick={this._toggleBurger.bind(this)}
+					>
 						<BurgerBar></BurgerBar>
 						<BurgerBar></BurgerBar>
 						<BurgerBar></BurgerBar>
 					</BurgerMenu>
-					<Col xs={12} md={6} p={0} style={{ alignSelf: 'center'}} >
+					<Col xs={12} md={6} p={0} style={{ alignSelf: 'center' }}>
 						<NavbarLinkContainer displayValue={burgerDisplayProp}>
 							<NavbarLink to="/brands">marques</NavbarLink>
 							<NavbarLink to="/help">aide</NavbarLink>
 							<NavbarLink to="/about">à propos</NavbarLink>
-							{authToken ? (
-								<UserBubble />
-							) : (
-								<NavbarLink to="/login" className="ml1 no-underline black">
-									connexion
-								</NavbarLink>
-							)}
+							<div className="d-none d-sm-block">
+								{authToken ? (
+									<UserBubble />
+								) : (
+									<NavbarLink to="/login" className="ml1 no-underline black">
+										connexion
+									</NavbarLink>
+								)}
+							</div>
 						</NavbarLinkContainer>
 					</Col>
 				</NavbarBox>
