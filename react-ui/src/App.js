797fb62c6fe1b385ba02d9a96bb68c77ea5e8bfc
Unflux <unflux.agency@gmail.com>
2019-08-20 14:29:25 +0200

    Add suggest brands modal + cta

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -7,7 +7,7 @@ import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import './styles/App.css'
 import 'intro.js/introjs.css'
-import 'intro.js/themes/introjs-custom.css'
+// import 'intro.js/themes/introjs-custom.css'
 import Navbar from './components/navbar/Navbar'
 import Home from './scenes/home/Home'
 import Search from './scenes/brands/Search'
@@ -19,36 +19,38 @@ import Deliberation from './scenes/deliberations/Deliberation'
 import UserProfile from './scenes/userProfile/UserProfile'
 import Login from './scenes/login/Login'
 import LostPanda from './scenes/404/LostPanda'
+import Suggest from './scenes/suggest/Suggest'
 
 // implements the routing thanks to react router dom. It loads the right
 // component by matching the path
 const App = () => (
-	<div className="App" id="App">
-		<Route path="/" component={Navbar} />
-		<Switch>
-			<Route
-				exact
-				strict
-				path="/:url*"
-				render={props => <Redirect to={`${props.location.pathname}/`} />}
-			/>
-			<Route exact path="/" component={Home} />
-			<Route exact path="/brands/" component={Search} />
-			<Route exact path="/help/" component={Help} />
-			<Route exact path="/about/" component={About} />
-			<Route exact path="/company/:companyId" component={Soul} />
-			<Route exact path="/company/:companyId/cause/:cause" component={Cause} />
-			<Route
-				exact
-				path="/company/:companyId/cause/:cause/act/:act"
-				component={Deliberation}
-			/>
-			<Route path="/user/" component={UserProfile} />
-			<Route path="/signup/" component={Login} />
-			<Route path="/login/" component={Login} />
-			<Route component={LostPanda} />
-		</Switch>
-	</div>
+  <div className="App" id="App">
+    <Route path="/" component={Navbar} />
+    <Switch>
+      <Route
+        exact
+        strict
+        path="/:url*"
+        render={props => <Redirect to={`${props.location.pathname}/`} />}
+      />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/brands/" component={Search} />
+      <Route exact path="/help/" component={Help} />
+      <Route exact path="/about/" component={About} />
+      <Route exact path="/company/:companyId" component={Soul} />
+      <Route exact path="/company/:companyId/cause/:cause" component={Cause} />
+      <Route
+        exact
+        path="/company/:companyId/cause/:cause/act/:act"
+        component={Deliberation}
+      />
+      <Route path="/user/" component={UserProfile} />
+      <Route path="/signup/" component={Login} />
+      <Route path="/login/" component={Login} />
+      <Route path="/suggest/" component={Suggest} />
+      <Route component={LostPanda} />
+    </Switch>
+  </div>
 )
 
 export default App
--- react-ui/src/scenes/brands/Search.js
+++ react-ui/src/scenes/brands/Search.js
@@ -11,7 +11,7 @@ import gql from 'graphql-tag'
 import SearchResult from './components/SearchResult'
 import styled from 'styled-components'
 import { Grid, Row, Col } from '@smooth-ui/core-sc'
-
+import { Link } from 'react-router-dom'
 // <STYLE>
 const Title = styled.div`
 	font-size: 30px;
@@ -36,41 +36,49 @@ const COMPANY_LIST = gql`
 // Search component: displays the list of companies in the database, calls
 // the SearchResult component on every item
 const Search = () => {
-	return (
-		// First fetch the data
-		<Query query={COMPANY_LIST}>
-			{({ loading, error, data }) => {
-				if (loading) return <div> Loading... </div>
-				if (error) return <div> Error </div>
+  return (
 
-				// companyList is an Object that contains the result of the query: id,
-				// name, logo of all companies, ordered by name
-				const companyList = data.allCompanies
 
-				//This maps each item in the object to a SearchResult component inside
-				//the grid that has layed out
-				return (
-					<Grid>
-						<Row justifyContent={{ md: 'center' }} mt={'96px'}>
-							<Col md={8}>
-								<Title>Les marques déjà sur Karma Panda :</Title>
-								<Row my={4} justifyContent={{ md: 'center' }}>
-									{companyList.map((company, i) => (
-										<SearchResult
-											key={i}
-											name={company.name}
-											id={company.id}
-											logo={company.logo}
-										/>
-									))}
-								</Row>
-							</Col>
-						</Row>
-					</Grid>
-				)
-			}}
-		</Query>
-	)
+
+    // First fetch the data
+    <Query query={COMPANY_LIST}>
+      {({ loading, error, data }) => {
+        if (loading) return <div> Loading... </div>
+        if (error) return <div> Error </div>
+
+        // companyList is an Object that contains the result of the query: id,
+        // name, logo of all companies, ordered by name
+        const companyList = data.allCompanies
+
+        //This maps each item in the object to a SearchResult component inside
+        //the grid that has layed out
+        return (
+
+
+
+          <Grid>
+
+            <Link to="/suggest">Voir les marques en attente d’évaluation</Link>
+            <Row justifyContent={{ md: 'center' }} mt={'96px'}>
+              <Col md={8}>
+                <Title>Les marques déjà sur Karma Panda :</Title>
+                <Row my={4} justifyContent={{ md: 'center' }}>
+                  {companyList.map((company, i) => (
+                    <SearchResult
+                      key={i}
+                      name={company.name}
+                      id={company.id}
+                      logo={company.logo}
+                    />
+                  ))}
+                </Row>
+              </Col>
+            </Row>
+          </Grid>
+        )
+      }}
+    </Query>
+  )
 }
 
 export default Search
